1deb459d5bb3c4de066f6a49ef9eeb0fadff4259
George Higbie <ghigbie@monigle.com>
2019-03-06 08:19:05 -0700

    Adjust range limit

--- magic_numbers.py
+++ magic_numbers.py
@@ -1,8 +1,8 @@
 import random
 
-range_limit = 10
+range_limit = 11 
 random = random.randint(1,range_limit)
-user_input = int(input(f"Please choose a number between 1 and {range_limit} > "))
+user_input = int(input(f"Please choose a number between 1 and {range_limit - 1} > "))
 
 while user_input != random:
     print('Please try again')
@@ -10,7 +10,7 @@ while user_input != random:
         print(f'Please choose a number that is higher than {user_input}')
     else:
         print(f'Please choose a number that is lover than {user_input}')
-    user_input = int(input(f"Please choose a number between 1 and {range_limit} > "))
+    user_input = int(input(f"Please choose a number between 1 and {range_limit - 1} > "))
 
 if user_input == random:
     print("You guessed it!")

7e220aa7b97805cb06c613fa80184defd8363093
George Higbie <ghigbie@monigle.com>
2019-03-07 20:37:58 -0700

    Edit vars

--- price_of_chair.py
+++ price_of_chair.py
@@ -1,5 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
+
 url = "https://icanhazdadjoke.com"
 
 response = requests.get(url)
-print(response.content)
+content = response.content
+soup = BeautifulSoup(content, 'html.parser')
+print(soup)

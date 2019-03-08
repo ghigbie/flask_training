cc1925d5a80498e0c3539ae1c3be10c39c0f4f85
George Higbie <ghigbie@monigle.com>
2019-03-07 20:54:40 -0700

    Able to get price

--- price_of_chair.py
+++ price_of_chair.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://icanhazdadjoke.com"
+url = "https://www.johnlewis.com/john-lewis-partners-santino-10-12-seater-extending-dining-table-solid-oak/p3945216"
+target_class = "price price--large"
 
 response = requests.get(url)
 content = response.content
 soup = BeautifulSoup(content, 'html.parser')
-print(soup)
+element = soup.find_all("p", {"class" : target_class} )
+print(f"The price of the table (not chair is {element.text}")
+
